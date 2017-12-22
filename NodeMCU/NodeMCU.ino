b0d1987b6e7b08dbab4d283683ec3d6edeb3f1c6
kubusbbt <kubusbbt@gmail.com>
2017-12-22 20:00:24 +0100

    first

---
+++ NodeMCU/NodeMCU.ino
@@ -0,0 +1,36 @@
+#define BLYNK_PRINT Serial
+
+
+/**** Biblioteki Blynk *****/
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+
+/**** Biblioteki RC switch *****/
+#include <RCSwitch.h>
+RCSwitch mySwitch = RCSwitch();
+
+
+char auth[] = "f012bd4309ee48a6b7c8046fc7c0e13b";
+char ssid[] = "JAdesign";
+char pass[] = "16031987";
+
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  Blynk.begin(auth, ssid, pass);
+
+  // Numer pinu do wysyłki danych RC
+  mySwitch.enableTransmit(D0);
+  
+  mySwitch.setPulseLength(206);
+}
+
+
+void loop()
+{
+  Blynk.run();
+}
+
